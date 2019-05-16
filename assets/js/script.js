ea692f4ee3486b016b48d2ac9c94da401b90dfc2
Camila Fuentealba <camila.fuentealba.rubilar@gmail.com>
2019-05-15 20:40:17 -0400

    agrega tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-$(function(){
-  $("a").cilck(function(event){
-    alert(this.hash);
+$(document).ready(function(){
+  $("a").click(function(event){
+    if(this.hash !== ""){
+      event.preventDefault();
+      var gato = this.hash;
+      $("html,body").animate({
+        scrollTop: $(gato).offset().top
+      },800,function(){
+        window.location.hash = gato;
+      });
+    }
   });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
 });
